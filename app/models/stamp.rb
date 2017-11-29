8b7c20e6e5a82c0a2a77ef3abf54a6e439a5997a
Martti <martti@aukia.com>
2017-11-29 17:52:25 +0000

    broken advanced search, needs fixing

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -17,11 +17,21 @@ class Stamp < ApplicationRecord
 
     # implement something like this: https://stackoverflow.com/questions/39691497/search-query-multiple-column-with-or-and-and-to-apply-filter
     def self.search(search)
-        where('name LIKE :search
-         OR colour LIKE :search
-         OR place_of_printing LIKE :search
-         OR artist LIKE :search OR currency LIKE :search', 
-         search: "%#{search}%").order(:release_date)
+        # all = Stamp.joins(:tags)
+        # all = all.where('stamps.name LIKE :search
+        #  OR colour LIKE :search
+        #  OR place_of_printing LIKE :search
+        #  OR artist LIKE :search OR currency LIKE :search
+        #  OR tags.name LIKE :search', 
+        #  search: "%#{search}%").order(:release_date)
+        all = Stamp.joins(:tags)
+        all = all.where('stamps.name LIKE :search', search: "%#{search[0]}%") if search[1]
+        all = all.where('colour LIKE :search', search: "%#{search[0]}%") if search[2]
+        all = all.where('place_of_printing LIKE :search', search: "%#{search[0]}%") if search[3]
+        all = all.where('artist LIKE :search', search: "%#{search[0]}%") if search[4] 
+        all = all.where('currency LIKE :search', search: "%#{search[0]}%") if search[5]
+        all = all.where('tags.name LIKE :search', search: "%#{search[0]}%") if search[6]
+        all.order!(:release_date)
     end
 
 end
